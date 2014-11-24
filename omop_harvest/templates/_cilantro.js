f31bcff4b31ad20ad6c4bab951244437bca61872
rv816 <ryan.vass.junk@gmail.com>
2014-11-24 15:38:58 -0500

    Upgrade templates to be compatible with Cilantro 2.3.8
    This follows the conventions of the Harvest OpenMRS demo repository(demo branch).

---
+++ omop_harvest/templates/_cilantro.js
@@ -0,0 +1,9 @@
+var require = {
+    baseUrl: '{{ STATIC_URL }}cilantro/js'
+},
+
+cilantro = {
+    url: '{% url "serrano:root" %}',
+    main: '#content',
+    root: '{{ request.META.SCRIPT_NAME }}'
+};
